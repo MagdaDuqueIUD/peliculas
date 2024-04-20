4dc32f79e16d870765cb313bac5e62d64b27d024
MagdaDuqueIUD <maria.duquemr@est.iudigital.edu.co>
2024-04-19 21:19:23 -0500

    Main nuevo

--- src/helpers/axios-config.js
+++ src/helpers/axios-config.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000/'
+//    baseURL: 'http://localhost:4000/'
+    baseURL: process.env.REACT_APP_BASE_URL
 });
 
 export {
